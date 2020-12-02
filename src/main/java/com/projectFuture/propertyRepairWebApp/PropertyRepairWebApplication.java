e0d32ebf04639998b2524e669d4e3b55135beb93
Vasilis Tsimpragakis <vtsimpra@gmail.com>
2020-12-02 23:49:26 +0200

    Initial version of the project.

---
+++ src/main/java/com/projectFuture/propertyRepairWebApp/PropertyRepairWebApplication.java
@@ -0,0 +1,13 @@
+package com.projectFuture.propertyRepairWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRepairWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyRepairWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectFuture/propertyRepairWebApp/PropertyRepairWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectFuture.propertyRepairWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRepairWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
