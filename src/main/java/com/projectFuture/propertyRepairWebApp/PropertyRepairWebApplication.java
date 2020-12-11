05028855d63cc0b8e6567f6e392600ec03c50f6c
hplap <theoxarhsbarnabastselikhs@gmail.com>
2020-12-11 20:57:22 +0200

    added create repair form

--- src/main/java/com/projectFuture/propertyRepairWebApp/PropertyRepairWebApplication.java
+++ src/main/java/com/projectFuture/propertyRepairWebApp/PropertyRepairWebApplication.java
@@ -1,30 +1,30 @@
 package com.projectFuture.propertyRepairWebApp;
 
-import com.projectFuture.propertyRepairWebApp.service.RepairService;
-import com.projectFuture.propertyRepairWebApp.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+//import com.projectFuture.propertyRepairWebApp.service.RepairService;
+//import com.projectFuture.propertyRepairWebApp.service.UserService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PropertyRepairWebApplication implements CommandLineRunner {
-	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private RepairService repairService;
+public class PropertyRepairWebApplication {
+//	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+//
+//	@Autowired
+//	private UserService userService;
+//	@Autowired
+//	private RepairService repairService;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(PropertyRepairWebApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
+//	@Override
+//	public void run(String... args) throws Exception {
 //		LOGGER.info("=============================");
 //		LOGGER.info("====Finding User by Id====");
 //		userService.findUser(1L).ifPresent(user -> LOGGER.info(user.toString()));
@@ -62,7 +62,7 @@ public class PropertyRepairWebApplication implements CommandLineRunner {
 //		repairService.getRepairsByUserId(2L).forEach(repair -> LOGGER.info(repair.toString()));
 //		LOGGER.info("=============================");
 
-	}
+//	}
 
 
 
